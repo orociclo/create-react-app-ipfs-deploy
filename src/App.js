58cb52da42d6ee71cc784a83adfdeb1178e46b0b
Óscar Romero <orociclo@gmail.com>
2021-05-20 11:43:33 +0200

    Added Orociclo

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <span className="Big-plus">+</span>
         <img src={reactLogo} className="React-logo" alt="react-logo" />
         <p>
-          React App deployed on IPFS through Fleek :) Now with pr deploys! Great IPFS!!
+          React App deployed on IPFS through Fleek :) Now with pr deploys! Orociclo
         </p>
         <a
           className="App-link"
